c13ab785c9e5682d6e3fe050855caecf076d5cf4
Egor Shchukin <vikzelikov@yandex.ru>
2025-02-27 01:50:24 +0300

    add haproxy to postgresql cluster

--- database/init-replica.sh
+++ database/init-replica.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 set -e
 
-echo "Running pg_basebackup..."
-pg_basebackup --pgdata=/var/lib/postgresql/data \
-              -R \
-              --host=postgres-master \
-              --port=5432
+PGDATA="/var/lib/postgresql/data"
+
+if [ "$(ls -A "$PGDATA" 2>/dev/null)" ]; then
+  echo "Data directory $PGDATA is not empty, skip pg_basebackup..."
+else
+  echo "Data directory $PGDATA is not empty, skip pg_basebackup..."
+  pg_basebackup \
+    --pgdata="$PGDATA" \
+    -R \
+    --host=postgres-master \
+    --port=5432
+fi
 
 echo "waiting for primary to connect..."
 sleep 5
 
 echo "Done, starting replica..."
-chmod 700 /var/lib/postgresql/data
+chmod 700 "$PGDATA"
 
 exec postgres
