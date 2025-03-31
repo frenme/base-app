cc368701e1f05dbf16165a917ca134dd766e53a6
Egor Shchukin <vikzelikov@yandex.ru>
2025-03-31 13:23:06 +0300

    bug fixes

--- app/order-service/internal/db/db.go
+++ app/order-service/internal/db/db.go
@@ -5,20 +5,46 @@ import (
 	"github.com/jackc/pgx/v4/pgxpool"
 	"go.mongodb.org/mongo-driver/mongo"
 	"log"
+	"log/slog"
 	"os"
 	"shared/pkg/db"
+	"shared/pkg/utils"
 	"time"
 )
 
 var PostgresMaster *pgxpool.Pool
 var PostgresReplicas *pgxpool.Pool
 var MongoClient *mongo.Client
+var logger *slog.Logger
+
+func init() {
+	handler := utils.LoggerHandler{Handler: slog.NewJSONHandler(os.Stdout, nil)}
+	logger = slog.New(handler)
+}
 
 func InitConnections() {
-	time.Sleep(120 * time.Second)
-	PostgresMaster = db.CreatePostgresPool(os.Getenv("POSTGRES_MASTER_CONNECTION"))
-	PostgresReplicas = db.CreatePostgresPool(os.Getenv("POSTGRES_REPLICAS_CONNECTION"))
-	MongoClient = db.CreateMongoClient(os.Getenv("MONGO_CONNECTION"))
+	maxAttempts := 100
+	for attempt := 1; attempt <= maxAttempts; attempt++ {
+		logger.Info("Attempt %d to initialize connections", attempt)
+
+		if PostgresMaster == nil || PostgresReplicas == nil {
+			PostgresMaster = db.CreatePostgresPool(os.Getenv("POSTGRES_MASTER_CONNECTION"))
+			PostgresReplicas = db.CreatePostgresPool(os.Getenv("POSTGRES_REPLICAS_CONNECTION"))
+		}
+		if MongoClient == nil {
+			MongoClient = db.CreateMongoClient(os.Getenv("MONGO_CONNECTION"))
+		}
+
+		if PostgresMaster != nil && PostgresReplicas != nil && MongoClient != nil {
+			break
+		}
+
+		time.Sleep(10 * time.Second)
+	}
+
+	if PostgresMaster == nil || PostgresReplicas == nil || MongoClient == nil {
+		logger.Error("Failed to initialize all connections after maximum attempts")
+	}
 }
 
 func CloseConnections() {
